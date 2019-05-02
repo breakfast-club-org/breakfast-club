38c7dd8762d62bd60c8195f15847ebbbcddb8d4e
Jason Ly <jason.ly@gmail.com>
2019-05-02 01:01:39 -0700

    feat: 🍈

--- 01-not-an-array.js
+++ 01-not-an-array.js
@@ -1,39 +1,41 @@
-// Implement a class called pseudoArray that has the following methods
+// Implement a class called notAnArray that has the following methods
 //
 // push
 //  - Accepts a value and places it at the front of the object with the key being an index based on the length and the value being the value passed into the function
-//  ex: 
-//  pseudoArray.push('first')
-//  pseudoArray.push('second')
+//  ex:
+//  notAnArray.push('first')
+//  notAnArray.push('second')
 //  should result in {0: 'first', 1: 'second'}
 //
 // pop
 //  - removes the value from the end of the object and returns it
-//  ex: 
-//  pseudoArray.pop();
+//  ex:
+//  notAnArray.pop();
 //  returns 'second' and the object should look like {0: 'first'}
 //
 // unshift
 //  - Accepts a value and places it at the end of the an object with the key being an index based on the length and the value being the value passed into the function
-//  ex: 
-//  pseudoArray.unshift('now I am first');
+//  ex:
+//  notAnArray.unshift('now I am first');
 //  should result in {0: 'now I am first', 1: 'first'}
 //
 // shift
 //  - removes the value from the front of the object and returns it
-//  ex: 
-//  pseudoArray.shift();
+//  ex:
+//  notAnArray.shift();
 //  returns 'now I am first' and the object should look like {0: 'first'}
 //
 
 class NotAnArray {
-  constructor() {}
-  
+  constructor() {
+    this.storage = {};
+  }
+
   push() {}
-  
+
   pop() {}
-  
+
   unshift() {}
-  
+
   shift() {}
 }
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Breakfast club
 
 A place for 🍩's and maybe 🍰's
+
+## Getting Started
+
+Cut a branch with whatever name you want but please have it in this format `yourNickname-solutions`. This branch will be the branch you'll be working off of -- just so we can keep things organized. You *don't* have to push your code up but it would be cool to be able to go through all the available solutions and compare answers.
+
+Maybe you have an awesome solution? Would be cool to share!
+
+I'll be posting the question of the week in this repository and you should be able to find the solution the following week or so in the `solutions` branch.
