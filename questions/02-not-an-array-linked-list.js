0a12198b798c0739421a4d75a7c3eac568ded018
Jason Ly <jason.ly@gmail.com>
2019-05-10 11:00:25 -0700

    refactor: add 📁 for questions

---
+++ questions/02-not-an-array-linked-list.js
@@ -0,0 +1,18 @@
+// Implement a class called NotAnArrayLinkedList...
+// All methods must be O(1)
+
+class NotAnArrayLinkedList {
+  constructor() {
+    this.storage = {};
+  }
+
+  push() {}
+
+  pop() {}
+
+  unshift() {}
+
+  shift() {}
+}
+
+module.exports = NotAnArrayLinkedList;
---
+++ test/02-not-an-array-linked-list.test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert;
+const NotAnArrayLinkedList = require('../questions/02-not-an-array-linked-list');
+
+describe('NotAnArrayLinkedList', () => {
+    describe('#Passing', () => {
+        it('should return true', () => {
+          return true;
+        });
+    });
+});
