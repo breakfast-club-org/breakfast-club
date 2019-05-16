53b58b1700bb1ddf9b67cf94b0c58333b5429b77
JasonLy <jason.ly@gmail.com>
2019-05-16 00:01:46 -0700

    feat: 🍍

--- questions/02-not-an-array-linked-list.js
+++ questions/02-not-an-array-linked-list.js
@@ -1,18 +1,50 @@
-// Implement a class called NotAnArrayLinkedList...
-// All methods must be O(1)
+// Implement a class called NotAnArrayLinkedList using the provided Node class
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+// push
+//  - accepts a value and places it at the tail of the linked list
+//
+// pop
+//  - returns and removes a value from the tail of the linked list
+//
+// unshift
+//  - accepts a value and places it at the head of the linked list
+//
+// shift
+//  - returns and removes a value from the head of the linked list
+//
+// [if you have time]
+// insert
+//  - accepts a value and position and inserts a node at the new position
+//
+// [if you have time]
+// deleteAt
+//  - accepts a position and deletes the node at that position
+//
 
 class NotAnArrayLinkedList {
   constructor() {
-    this.storage = {};
+    this.head = null;
+    this.length = 0;
   }
 
   push() {}
 
-  pop() {}
+  shift() {}
 
   unshift() {}
 
-  shift() {}
+  pop() {}
+
+  insert() {}
+
+  deleteAt() {}
 }
 
 module.exports = NotAnArrayLinkedList;
--- test/02-not-an-array-linked-list.test.js
+++ test/02-not-an-array-linked-list.test.js
@@ -2,9 +2,99 @@ const assert = require('chai').assert;
 const NotAnArrayLinkedList = require('../questions/02-not-an-array-linked-list');
 
 describe('NotAnArrayLinkedList', () => {
-    describe('#Passing', () => {
-        it('should return true', () => {
-          return true;
+    describe('#push()', () => {
+        it('should add to the tail', () => {
+          const o = new NotAnArrayLinkedList();
+
+          o.push('never');
+          o.push('gonna');
+          o.push('give');
+          o.push('you');
+          o.push('up');
+
+          const stringO = JSON.stringify(o);
+          const mock = '{"head":{"element":"never","next":{"element":"gonna","next":{"element":"give","next":{"element":"you","next":{"element":"up","next":null}}}}},"length":5}';
+
+          assert.equal(o.length, 5);
+          assert.equal(stringO, mock);
         });
     });
+
+    describe('#pop()', () => {
+        it('should return element from the tail', () => {
+          const o = new NotAnArrayLinkedList();
+
+          o.push('never');
+          o.push('gonna');
+          o.push('give');
+          o.push('you');
+          o.push('up');
+          o.pop();
+          o.pop();
+          o.pop();
+          o.pop();
+
+          const finalElement = o.pop();
+          const stringO = JSON.stringify(o);
+          const mock = '{"head":null,"length":0}';
+
+          assert.equal(finalElement.element, 'never');
+          assert.equal(o.length, 0);
+          assert.equal(stringO, mock);
+        });
+    });
+
+    describe('#unshift()', () => {
+        it('should add to the head', () => {
+          const o = new NotAnArrayLinkedList();
+
+          o.unshift('never');
+          o.unshift('gonna');
+          o.unshift('give');
+          o.unshift('you');
+          o.unshift('up');
+
+          const stringO = JSON.stringify(o);
+          const mock = '{"head":{"element":"up","next":{"element":"you","next":{"element":"give","next":{"element":"gonna","next":{"element":"never","next":null}}}}},"length":5}';
+
+          assert.equal(o.length, 5);
+          assert.equal(stringO, mock);
+        });
+    });
+
+    describe('#shift()', () => {
+        it('should remove the element from the head and return it', () => {
+          const o = new NotAnArrayLinkedList();
+
+          o.unshift('never');
+          o.unshift('gonna');
+          o.unshift('give');
+          o.unshift('you');
+          o.unshift('up');
+          o.shift();
+          o.shift();
+          o.shift();
+          o.shift();
+
+          const finalElement = o.shift();
+          const stringO = JSON.stringify(o);
+          const mock = '{"head":null,"length":0}';
+
+          assert.equal(finalElement.element, 'never');
+          assert.equal(o.length, 0);
+          assert.equal(stringO, mock);
+        });
+    });
+
+    // describe('#insert()', () => {
+    //     it('should have some tests', () => {
+    //       // your own test here
+    //     });
+    // });
+    //
+    // describe('#deleteAt()', () => {
+    //     it('should have some tests', () => {
+    //       // your own test here
+    //     });
+    // });
 });
