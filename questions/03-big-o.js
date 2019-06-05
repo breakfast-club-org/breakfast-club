22a1e286c73d1c489f5d4d9ad0b0fd3a3da45b13
Jason Ly <jason.ly@gmail.com>
2019-06-05 15:00:11 -0700

    feat: solution for big o ⌛

--- questions/03-big-o.js
+++ questions/03-big-o.js
@@ -50,24 +50,24 @@
 
 const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// (1) Time Complexity:
+// (1) Time Complexity: O(1)
 const logFirstValue = (n) => {
   console.log(n[0])
 }
 
-// (2) Time Complexity:
+// (2) Time Complexity: O(1)
 const logLastValue = (n) => {
   console.log(n[n.length - 1])
 }
 
-// (3) Time Complexity:
+// (3) Time Complexity: O(n)
 const logAllValues = (n) => {
   for (let i = 0; i < n.length; i++) {
     console.log(n[i])
   }
 }
 
-// (4) Time Complexity:
+// (4) Time Complexity: O(n^2)
 const logCombinations = (n) => {
   for (let i = 0; i < n.length; i++) {
     for (let j = 0; j < n.length; j++) {
@@ -76,7 +76,7 @@ const logCombinations = (n) => {
   }
 }
 
-// (5) Time Complexity:
+// (5) Time Complexity: O(n log n)
 const binarySearch = (arr, target) => {
   let startIndex = 0;
   let endIndex = arr.length - 1;
@@ -95,7 +95,7 @@ const binarySearch = (arr, target) => {
   return false;
 }
 
-// (6) Time Complexity:
+// (6) Time Complexity: O(n^2)
 const quickSort = list => {
   if (list.length < 2) {
     return list;
@@ -120,12 +120,12 @@ const quickSort = list => {
   ];
 };
 
-// (7) Time Complexity:
+// (7) Time Complexity: O(1)
 const firstElemEven = (array) => {
   return array[0] % 2 === 0 ? true : false;
 }
 
-// (8) Time Complexity:
+// (8) Time Complexity: O(n)
 const hasValue = (array, value) => {
   for (var i = 0; i < array.length; i++){
     if (array[i] === value){
@@ -136,7 +136,7 @@ const hasValue = (array, value) => {
   return false;
 }
 
-// (9) Time Complexity:
+// (9) Time Complexity: O(n^2)
 const findMatch = (string) => {
   for (var i = 0; i < string.length; i++){
     for ( var j = i+1; j < string.length; j++){
@@ -149,19 +149,19 @@ const findMatch = (string) => {
   return false;
 }
 
-// (10) Time Complexity:
+// (10) Time Complexity: O(1)
 data.pop()
 
-// (11) Time Complexity:
+// (11) Time Complexity: O(n)
 data.shift()
 
-// (12) Time Complexity:
+// (12) Time Complexity: O(n)
 data.unshift()
 
-// (13) Time Complexity:
+// (13) Time Complexity: O(1)
 data.push()
 
-// (14) Time Complexity:
+// (14) Time Complexity: O(n)
 const merge = (left, right) => {
   let resultArray = [];
   let leftIndex = 0
@@ -182,7 +182,7 @@ const merge = (left, right) => {
     .concat(right.slice(rightIndex));
 }
 
-// (15) Time Complexity:
+// (15) Time Complexity: O(n log n)
 const mergeSort = (unsortedArray) => {
   if (unsortedArray.length <= 1) {
     return unsortedArray;
@@ -198,27 +198,68 @@ const mergeSort = (unsortedArray) => {
 }
 
 // (16) Time Complexity:
+// if run for the first time then O(n!) or like O(n^2)
+// all subsequent times that are below the largest run time are O(n) if you memoize
 // Finding the factorial of a number
-const factorial = (n) => {
+let cache = {};
 
-}
+const factorial = (x) => {
+  if (x in cache) {
+    return cache[x];
+  }
 
-// (17) Time Complexity:
+  if (x === 0) {
+  	return 1;
+  } else {
+    let result = x * factorial(x - 1);
+    cache[x] = result;
+
+  	return result;
+  }
+};
+
+// (17) Time Complexity: O(n^2)
 // Finding all distinct subsets of a given string
 // subSetsOfAString('') outputs  ['']
 // subSetsOfAString('a') outputs ['', 'a']
 // subSetsOfAString('ab') outputs ['', 'a', 'b', 'ab']
 const subSetsOfAString = (n) => {
+  const arr = Array.from(n);
+  const cache = [''];
 
+  const results = arr.reduce((previous, element) => {
+    const previousPlusElement = previous.map((el) => {
+      return `${el}${element}`;
+    });
+
+    return previous.concat(previousPlusElement);
+  }, cache);
+
+  return results;
 }
 
-// (18) Time Complexity:
+
+// (18) Time Complexity: O(n!) or like O(n^2)
 // Write a function that computes all the different words that can be formed given a string
 // permutationsOfAString('a') outputs ['a']
 // permutationsOfAString('ab') outputs  ['ab', 'ba']
 // permutationsOfAString('abc') outputs ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
-const permutationsOfAString = (string, prefix = '') => {
+const permutationsOfAString = (str, prefix = '') => {
+  if (str.length <= 1) {
+    let permutation = `${prefix}${str}`;
 
+    return [permutation];
+  }
+
+  const arrCopy = Array.from(str);
+
+  return arrCopy.reduce((result, char, index) => {
+    const leftOver = `${str.slice(0, index)}${str.slice(index+1)}`;
+
+    result = result.concat(permutationsOfAString(leftOver, prefix + char));
+
+    return result;
+  }, []);
 }
 
 module.exports = {
