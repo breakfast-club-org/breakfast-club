9ccd646cb4759e6a92611794ad9523b4ec4f510e
Jason Ly <jason.ly@gmail.com>
2019-12-12 11:19:39 -0800

    maze runner

--- questions/11-maze-runner.js
+++ questions/11-maze-runner.js
@@ -17,7 +17,38 @@ class MazeRunner {
 	constructor() {}
 
 	// return true if the maze is solveable
-	findEnd(arr) {}
+	findEnd(arr) {
+		let maze = arr;
+		let isSolveable = false;
+
+		const traverseMaze = function(col, row) {
+			if (maze[col][row] === 'f') { // goal
+				isSolveable = true;
+			} else if (maze[col][row] === 's' || maze[col][row] === 0) { // path
+				maze[col][row] = 'visited';
+
+				if (col > 0) {
+					traverseMaze(col - 1, row); // check up
+				}
+
+				if (row < maze[col].length - 1) {
+					traverseMaze(col, row + 1); // check right
+				}
+
+				if (col < maze.length - 1) {
+					traverseMaze(col + 1, row); // check down
+				}
+
+				if (row > 0) {
+					traverseMaze(col, row - 1); // check left
+				}
+			}
+		};
+
+		traverseMaze(0, 0);
+
+		return isSolveable;
+	}
 }
 
 module.exports = MazeRunner;
