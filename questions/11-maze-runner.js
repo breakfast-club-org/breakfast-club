57294384f4cb0ebeca4501dab9b72bc721e0828a
Shawn Zhu <shawn.zhu@yahoo.com>
2019-12-12 11:41:49 -0800

    solution for maze runner

--- questions/11-maze-runner.js
+++ questions/11-maze-runner.js
@@ -1,6 +1,6 @@
 // Given a 2d array where
 // 's' represents the start
-// 'e' represents the end
+// 'f' represents the end
 // 0 represents a path
 // 1 represents a wall
 // determine if the maze is solveable and return true or false
@@ -12,12 +12,66 @@
 // 	[0, 1, 0, 0, 1],
 // 	[0, 1, 1, 0, 'f']
 // ] // should return true
+const END = 'f';
 
 class MazeRunner {
-	constructor() {}
+	constructor() {
+	}
 
 	// return true if the maze is solveable
-	findEnd(arr) {}
+	findEnd(maze) {
+		this.maze = maze;
+		return this.visit(0, 0);
+	}
+
+	isValid(x, y) {
+		const r = this.maze[x] !== undefined && this.maze[x][y] !== undefined;
+
+		return r;
+	}
+
+	spotValue(row, column) {
+		return this.maze[row][column];
+	}
+
+	visit(row, column) {
+		if (!this.isValid(row, column)) { return false; }
+
+		let spotValue = this.spotValue(row, column);
+
+		if (spotValue === 'x') { return false; } // visited
+		if (spotValue === 1) { return false }
+
+		this.maze[row][column] = 'x'; // mark visited
+		if (spotValue === END) { return true; }
+
+		//
+		// else check all directions
+		//
+
+		// else
+		// check right
+		if (this.visit(row, column + 1)) {
+			return true;
+		}
+
+		// check up
+		if (this.visit(row - 1, column)) {
+			return true;
+		}
+
+		// check down
+		if (this.visit(row + 1, column)) {
+			return true;
+		}
+
+		// left
+		if (this.visit(row, column - 1)) {
+			return true;
+		}
+
+		return false;
+	}
 }
 
 module.exports = MazeRunner;
--- test/11-maze-runner.test.js
+++ test/11-maze-runner.test.js
@@ -16,7 +16,28 @@ describe('Matrix Transformations', () => {
 
 			assert.isTrue(mazeRunner.findEnd(input), 'Yay!');
 		});
+		it('it should return true if it is solveable', () => {
+			const input = [
+				['s', 0, 0, 0, 0],
+				[1, 0, 1, 1, 0],
+				[0, 0, 0, 0, 0],
+				[0, 1, 1, 1, 1],
+				[0, 0, 0, 0, 'f']
+			]
 
+			assert.isTrue(mazeRunner.findEnd(input), 'Yay!');
+		});
+		it('it should return true if it is solveable', () => {
+			const input = [
+				['s', 0, 0, 0, 0],
+				[0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 'f']
+			]
+
+			assert.isTrue(mazeRunner.findEnd(input), 'Yay!');
+		});
 		it('it should return false if it is not solveable', () => {
 			const input = [
 				['s', 0, 0, 0, 0],
@@ -24,6 +45,17 @@ describe('Matrix Transformations', () => {
 				[0, 0, 0, 0, 0],
 				[0, 0, 0, 0, 0],
 				[0, 0, 0, 0, 'f']
+			]
+
+			assert.isFalse(mazeRunner.findEnd(input), 'Boo!');
+		});
+		it('it should return false if it is not solveable', () => {
+			const input = [
+				['s', 0, 0, 0, 0],
+				[1, 1, 1, 0, 1],
+				[0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 1],
+				[0, 0, 0, 1, 'f']
 			]
 
 			assert.isFalse(mazeRunner.findEnd(input), 'Boo!');
