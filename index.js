4a75332bef2d8a489c8f0b2921760edcfe1cf0b6
Katherine Mitchell <katherine_m@apple.com>
2020-01-24 10:41:27 -0800

    Comments out the Matrix question

--- index.js
+++ index.js
@@ -10,38 +10,38 @@ app.get('/', function(req, res){
 app.listen(3000);
 
 
-const matrixTransforms = require('./questions/10-matrix-transformations');
+// const matrixTransforms = require('./questions/10-matrix-transformations');
 
-const input = [
-	[1, 2, 3],
-	[4, 5, 6],
-	[7, 8, 9]
-]
+// const input = [
+// 	[1, 2, 3],
+// 	[4, 5, 6],
+// 	[7, 8, 9]
+// ]
 
-const fourbythree = [
-	[1, 2, 3, 4],
-	[5, 6, 7, 8],
-	[9, 10, 11, 12]
-]
+// const fourbythree = [
+// 	[1, 2, 3, 4],
+// 	[5, 6, 7, 8],
+// 	[9, 10, 11, 12]
+// ]
 
-const threebyfour = [
-		[1, 5, 9],
-		[2, 6, 10],
-		[3, 7, 11],
-		[4, 8, 12]
-]
+// const threebyfour = [
+// 		[1, 5, 9],
+// 		[2, 6, 10],
+// 		[3, 7, 11],
+// 		[4, 8, 12]
+// ]
 
-const threebythree = [
-		[1, 2, 3],
-		[4, 5],
-		[6, 7, 8]
-]
+// const threebythree = [
+// 		[1, 2, 3],
+// 		[4, 5],
+// 		[6, 7, 8]
+// ]
 
-const expectedOutputthreebythree = [
-	[1, 4, 6],
-	[2, 5, 7],
-	[3, null, 8]
-]
+// const expectedOutputthreebythree = [
+// 	[1, 4, 6],
+// 	[2, 5, 7],
+// 	[3, null, 8]
+// ]
 
 // console.log(threebyfour);
 
