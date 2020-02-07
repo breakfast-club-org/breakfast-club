65cc2c89c7e103d95ed293d398d6852fa05e6aa9
Shawn Zhu <shawn.zhu@yahoo.com>
2020-02-07 10:22:32 -0800

    linear mergeSortedArray implemenation

--- questions/13-merge-sorted-arrays.js
+++ questions/13-merge-sorted-arrays.js
@@ -8,8 +8,39 @@
 // [1, 2, 2, 3, 3, 4, 5, 6, 6, 7, 8, 9, 12, 18, 19, 22, 22, 23, 23, 99]
 
 const mergeSortedArrays = (arr1, arr2) => {
-  // code here but don't do this
-  return [...arr1, ...arr2].sort(); //O(nlogn) //O(n)
+  const iter1 = arr1.values();
+  const iter2 = arr2.values();
+
+  let n1 = iter1.next();
+  let n2 = iter2.next();
+  const result = [];
+
+  while (!(n1.done || n2.done)) {
+    if (n1.value < n2.value) {
+      result.push(n1.value);
+      n1 = iter1.next();
+    } else if (n1.value > n2.value) {
+      result.push(n2.value);
+      n2 = iter2.next();
+    } else {
+      result.push(n1.value);
+      result.push(n2.value);
+      n1 = iter1.next();
+      n2 = iter2.next();
+    }
+  }
+
+  while (!n1.done) {
+    result.push(n1.value);
+    n1 = iter1.next();
+  }
+
+  while (!n2.done) {
+    result.push(n2.value);
+    n2 = iter2.next();
+  }
+
+  return result;
 };
 
 module.exports = mergeSortedArrays;
--- test/13-merge-sorted-arrays.test.js
+++ test/13-merge-sorted-arrays.test.js
@@ -2,16 +2,33 @@ const assert = require('chai').assert;
 const mergeSortedArrays = require('../questions/13-merge-sorted-arrays');
 
 describe('Merge Sorted Arrays', () => {
-	describe('findEnd', () => {
-		it('it should return a 2d array with the shortest possible path', () => {
-      const arr1 = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 23, 23];
-      const arr2 = [2, 3, 12, 18, 19, 22, 22, 99];
+	describe('two presorted arrays', () => {
+		it('it should return a merged-sorted array', () => {
+			const arr1 = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 23, 23];
+			const arr2 = [2, 3, 12, 18, 19, 22, 22, 99];
 
-      const expectedResult = [1, 2, 2, 3, 3, 4, 5, 6, 6, 7, 8, 9, 12, 18, 19, 22, 22, 23, 23, 99];
-
-      mergeSortedArrays(arr1, arr2);
+			const expectedResult = [1, 2, 2, 3, 3, 4, 5, 6, 6, 7, 8, 9, 12, 18, 19, 22, 22, 23, 23, 99];
 
 			assert.deepEqual(mergeSortedArrays(arr1, arr2), expectedResult);
 		});
+
+		it('it should return a merged-sorted array when one of the array is empty', () => {
+			const arr1 = [];
+			const arr2 = [2, 3, 12, 18, 19, 22, 22, 99];
+
+			const expectedResult = arr2;
+
+			assert.deepEqual(mergeSortedArrays(arr1, arr2), expectedResult);
+		});
+
+		it('it should return a merged-sorted array when both of the arrays are empty', () => {
+			const arr1 = [];
+			const arr2 = [];
+
+			const expectedResult = [];
+
+			assert.deepEqual(mergeSortedArrays(arr1, arr2), expectedResult);
+		});
+
 	});
 });
