e7e19ebe6b54e3add319e83d5b936df655bfa86c
Jason Ly <jason.ly@gmail.com>
2020-02-26 15:45:08 -0800

    find dups

---
+++ questions/14-find-dups.js
@@ -0,0 +1,13 @@
+// Given an array of numbers that can only be duplciated once, return an array of the duplciates in the order you found them in
+// TIME: O(n)
+// SPACE: O(1) I think?
+// For example if you had an array such as
+// const arr = [1, 2, 2, 3, 1, 4, 6, 5, 6, 4];
+// the result should be
+// [2, 1, 6, 4]
+
+const findDups = (arr) => {
+
+}
+
+module.exports = findDups;
