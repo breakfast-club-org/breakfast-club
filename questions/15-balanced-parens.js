d64b01ca61a5e6fb9ae98b8d9d5e585b427f5937
Jason Ly <jason.ly@gmail.com>
2020-03-19 11:19:14 -0700

    typo fix

--- questions/15-balanced-parens.js
+++ questions/15-balanced-parens.js
@@ -1,4 +1,4 @@
-// Given given a string of parens and return true if the parens are balanced. If a string has no length it's technically balanced (Edge cases are the worst don't eve). Please keep in mind time and space complexity.
+// Given given a string of parens and return true if the parens are balanced. If a string has no length it's technically balanced (Edge cases are the worst don't even). Please keep in mind time and space complexity.
 // TIME: O(n)
 // SPACE: O(1)
 // For example
