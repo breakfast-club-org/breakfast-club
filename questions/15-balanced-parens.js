06cc097d5d5e5d8befd4cc0cb502d0e767d16df2
Jason Ly <jason.ly@gmail.com>
2020-04-02 10:33:09 -0700

    ()()()()))()()() maybe this is true

--- questions/15-balanced-parens.js
+++ questions/15-balanced-parens.js
@@ -6,7 +6,15 @@
 // const parens = "()(())(" // should be false
 
 const balancedParens = (expression) => {
+	const thing = expression.split('').reduce((acc, curr) => {
+		if (curr === '(') {
+			return acc = acc + 1;;
+		} else if (curr === ')') {
+			return acc = acc - 1;
+		}
+	}, 0);
 
+	return thing === 0;
 }
 
 module.exports = balancedParens;
