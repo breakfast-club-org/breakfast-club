f372d82875de56ee58183fb0007bd4b6cb189b65
Jason Ly <jason.ly@gmail.com>
2020-06-25 17:15:24 -0700

    box

---
+++ questions/17-storage-center.js
@@ -0,0 +1,12 @@
+// Write a class called storage center that has three methods set, has, and get
+// set should accept a node and a value and store it the data store
+// has should check if the node exists within your data store
+// get should retrieve the value associated with the node that was stored
+
+class StorageCenter {
+  constructor(){}
+
+  set(node, val) {}
+  has(node) {}
+  get(node) {}
+}
