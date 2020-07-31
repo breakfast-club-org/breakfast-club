85f565fc4d97415e617e709106513ded8e1f4fd9
Shawn Zhu <shawn.zhu@yahoo.com>
2020-07-31 15:00:16 -0700

    fix some more tests

--- test/18-top-down-left-right.test.js
+++ test/18-top-down-left-right.test.js
@@ -1,20 +1,21 @@
 const assert = require('chai').assert;
 const tdlr = require('../questions/18-top-down-left-right');
 
-describe('Storage Center!', () => {
-	describe('x and y should return an array', () => {
-    assert.equal(Array.isArray(tdlr(2, 2)), true);
-    assert.equal(Array.isArray(tdlr(3, 3)), true);
-    assert.equal(Array.isArray(tdlr(4, 4)), true);
+describe('tdlr!', () => {
 
-    assert.notEqual(Array.isArray(tdlr(2, 3)), false);
-	});
-
-  describe('tdlr(1, 1) should be null', () => {
-    assert.equal(tdlr(1, 1), null);
+  it('tdlr(1, 1) should be null', () => {
+    assert.isNull(tdlr(1, 1));
   });
 
-  describe('tdlr(2, 2) should be in the correct order', () => {
+	it('x and y should return an array', () => {
+    assert.isTrue(Array.isArray(tdlr(2, 2)));
+    assert.isTrue(Array.isArray(tdlr(3, 3)));
+    assert.isTrue(Array.isArray(tdlr(4, 4)));
+
+    assert.isFalse(Array.isArray(tdlr(2, 3)));
+	});
+
+  it('tdlr(2, 2) should be in the correct order', () => {
     const output = [
       [1, 3],
       [2, 4]
@@ -23,7 +24,7 @@ describe('Storage Center!', () => {
     assert.equal(tdlr(2, 2), output);
   });
 
-  describe('tdlr(3, 3) should be in the correct order', () => {
+  it('tdlr(3, 3) should be in the correct order', () => {
     const output = [
       [1, 4, 7],
       [2, 5, 8],
