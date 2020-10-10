c5a32ee5a80217e5bfe64245b7a8b8d156010c30
Shawn Zhu <shawn.zhu@yahoo.com>
2020-10-09 17:22:18 -0700

    update test cases

--- test/19-enhanced-add.test.js
+++ test/19-enhanced-add.test.js
@@ -1,9 +1,28 @@
 const assert = require('chai').assert;
 const add = require('../questions/19-enhanced-add');
 
-describe('enhanced add function tests', function () {
-	it('should support two different invocation', function () {
-		assert.equal(add(1)(2)(3), 6);
-		assert.equal(add(1,2,3), 6);
+describe('enhanced add function tests', function() {
+
+	describe('single argument format', function() {
+
+		it('should return 0 if first call without params', function() {
+			assert.equal(add(), 0);
+		});
+
+		it('should return n when called once', function() {
+			assert.equal(add(1)(), 1);
+			assert.equal(add(2)(), 2);
+		});
+
+		it('should support multiple calls', function() {
+			assert.equal(add(1)(2)(3)(), 6);
+		});
+	});
+
+	describe('mutliple argument format', function() {
+
+		it('should return the sum of the arugment list', function() {
+			assert.equal(add(1, 2, 3), 6);
+		});
 	});
 });
