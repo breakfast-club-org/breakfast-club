db33c627a9fda2a2573efb8fa46679c8ffc55620
Jason Ly <jason.ly@gmail.com>
2020-10-15 20:04:34 -0700

    oh almost forgot to do this

--- questions/19-enhanced-add.js
+++ questions/19-enhanced-add.js
@@ -3,6 +3,21 @@
  * 1) single argument: add(1)(2)...
  * 2) argument list: add(1,2,3...)
  */
-const add = () => {
-	/** your implementation */
+
+const add = function () {
+    let args = [...arguments];
+
+    function closureAdd(){
+      args = [...args, ...arguments];
+
+      if (args.length >= 3) {
+        return args.slice(0, args.length).reduce((acc, next) => { return acc + next}, 0);
+      }
+
+      return closureAdd;
+    }
+
+    return closureAdd();
 }
+
+module.exports = add;
