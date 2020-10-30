d8a73e676df480c8566d2ef4973bbebe355ba029
Jason Ly <jason.ly@gmail.com>
2020-10-30 09:39:45 -0700

    tabs v space

--- test/20-browser-history.js
+++ test/20-browser-history.js
@@ -2,18 +2,18 @@ const assert = require('chai').assert;
 const BrowserHistory = require('../questions/20-browser-history');
 
 describe('Browser History', function() {
-	it('should return an object with some methods on it', function() {
+  it('should return an object with some methods on it', function() {
     const bh = new BrowserHistory();
 
-		// assert.equal(TypeOf bh.historyStore, 'some storage'); // this one is up to you
-	});
+    // assert.equal(TypeOf bh.historyStore, 'some storage'); // this one is up to you
+  });
 
   it('should visit a page', function() {
     const bh = new BrowserHistory();
     bh.visit('www.apple.com');
 
-		assert.equal(bh.visit('www.apple.com'), 'You are at www.apple.com');
-	});
+    assert.equal(bh.visit('www.apple.com'), 'You are at www.apple.com');
+  });
 
   it('should go back a few pages in history', function() {
     const bh = new BrowserHistory();
@@ -21,10 +21,10 @@ describe('Browser History', function() {
     bh.visit('www.reddit.com');
     bh.visit('www.kotaku.com');
 
-		assert.equal(bh.back(1), 'You are at www.reddit.com');
+    assert.equal(bh.back(1), 'You are at www.reddit.com');
     assert.equal(bh.back(1), 'You are at www.apple.com');
     assert.equal(bh.back(1), 'You are at www.apple.com');
-	});
+  });
 
   it('should go forward a few pages in history', function() {
     const bh = new BrowserHistory();
