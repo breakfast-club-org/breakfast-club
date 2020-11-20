6f763c428675ba25ae9be9f6b65db65a4c4edc24
Shawn Z <shawn.zhu@yahoo.com>
2020-11-20 10:53:38 -0800

    Shawn solutions (#39)
    * feat: 🃏
    
    * feat: 🃏
    
    * refactor: remove schedule
    
    * wo
    
    * add fisher-yates shuffle()
    
    * feat: woo methods and stuff
    
    * update shuffle to be an object instead of a class. add deck implementation
    
    * refactor: casino
    
    * Fix styles
    
    * update casino
    
    * 🎵
    
    * music
    
    * matrix transformations
    
    * maze runner not the book
    
    * cleanup
    
    * more clean up
    
    * tabs vs spaces
    
    * solution for maze runner
    
    * wow
    
    * wow
    
    * don't
    
    * linear mergeSortedArray implemenation
    
    * find dups
    
    * next next week
    
    * docs(readme): Updating with "Resources" section (#25)
    
    * typo fix
    
    * solution
    
    * solution for paren balance check
    
    * add some edge cases
    
    * averages
    
    * r
    
    * solution for 16-target-average
    
    * box
    
    * solutions for 17-storage-center
    
    * chore(deps): bump lodash from 4.17.11 to 4.17.19 (#29)
    
    Bumps [lodash](https://github.com/lodash/lodash) from 4.17.11 to 4.17.19.
    - [Release notes](https://github.com/lodash/lodash/releases)
    - [Commits](https://github.com/lodash/lodash/compare/4.17.11...4.17.19)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * tldr
    
    * tdlr not tldr
    
    * fix tests for 18-tdlr
    
    * minor fix to the tests (#31)
    
    * implement push and pop
    
    * fix tests for 18-tdlr
    
    * restore master files
    
    * restoring two files on master (#32)
    
    * implement push and pop
    
    * fix tests for 18-tdlr
    
    * restore master files
    
    * initial impl
    
    * fix some more tests
    
    * update test cases (#33)
    
    * implement push and pop
    
    * fix tests for 18-tdlr
    
    * restore master files
    
    * fix some more tests
    
    * need to use deepEqual to compare matrix arrays
    
    * fix tests (#34)
    
    * implement push and pop
    
    * fix tests for 18-tdlr
    
    * restore master files
    
    * fix some more tests
    
    * need to use deepEqual to compare matrix arrays
    
    * minor syntax change
    
    * add enhanced add function
    
    * enhanced add question (#36)
    
    * implement push and pop
    
    * fix tests for 18-tdlr
    
    * restore master files
    
    * fix some more tests
    
    * need to use deepEqual to compare matrix arrays
    
    * add enhanced add function
    
    * update test cases
    
    * fix test cases; and properly export function
    
    * add solution to #19
    
    * fix test cases throw stack trace
    
    * updated test cases (#37)
    
    * implement push and pop
    
    * fix tests for 18-tdlr
    
    * restore master files
    
    * fix some more tests
    
    * need to use deepEqual to compare matrix arrays
    
    * add enhanced add function
    
    * update test cases
    
    * fix test cases; and properly export function
    
    * fix test cases throw stack trace
    
    * add curry implementation for question 19 variant
    
    * bh
    
    * refactor: security fix
    
    * refactor: security update
    
    * tabs v space
    
    * solution for 20-browser-history
    
    Co-authored-by: Jason Ly <jason.ly@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ test/20-browser-history.js
@@ -0,0 +1,51 @@
+const assert = require('chai').assert;
+const BrowserHistory = require('../questions/20-browser-history');
+
+describe('Browser History', function() {
+  it('should return an object with some methods on it', function() {
+    const bh = new BrowserHistory();
+
+    assert.isArray(bh.history); // this one is up to you
+  });
+
+  it('should visit a page', function() {
+    const bh = new BrowserHistory();
+
+    assert.equal(bh.visit('www.apple.com'), 'You are at www.apple.com');
+  });
+
+  it('should go back a few pages in history', function() {
+    const bh = new BrowserHistory();
+    bh.visit('www.apple.com');
+    bh.visit('www.reddit.com');
+    bh.visit('www.kotaku.com');
+
+    assert.equal(bh.back(1), 'You are at www.reddit.com');
+    assert.equal(bh.back(1), 'You are at www.apple.com');
+    assert.equal(bh.back(1), 'You are at www.apple.com');
+  });
+
+  it('should go forward a few pages in history', function() {
+    const bh = new BrowserHistory();
+    bh.visit('www.apple.com');
+    bh.visit('www.reddit.com');
+    bh.visit('www.kotaku.com');
+    bh.back(1);
+    bh.back(1);
+
+    assert.equal(bh.forward(1), 'You are at www.reddit.com');
+    assert.equal(bh.forward(1), 'You are at www.kotaku.com');
+    assert.equal(bh.forward(1), 'You are at www.kotaku.com');
+  });
+
+  it('should erase history going forward if you visit a page in the middle of history', function() {
+    const bh = new BrowserHistory();
+    bh.visit('www.apple.com');
+    bh.visit('www.reddit.com');
+    bh.visit('www.kotaku.com');
+    bh.back(1); // www.reddit.com
+    bh.visit('www.youtube.com'); // 'You are at www.youtube.com';
+
+    assert.equal(bh.forward(1), 'You are at www.youtube.com');
+  });
+});
