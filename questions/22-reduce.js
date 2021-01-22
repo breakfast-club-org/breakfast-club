16dd24d5ada6edbed3f5f2d9997a96ab823c3b76
Brian Thiel <bthiel@apple.com>
2021-01-22 11:19:02 -0600

    simple sumReducer passed to reduce

--- questions/22-reduce.js
+++ questions/22-reduce.js
@@ -5,10 +5,24 @@
  * where:
  *   collection - array like data structure
  * 	 reducer - function
- *   initial - optional intial value
+ *   initial - optional initial value
  */
-const reduce = (collection, reducer, intial) => {
-	/* implmentation */
+
+const collection = [1, 2, 3, 4, 5];
+const initial = 0;
+const sumReducer = (acc, val) => acc + val;
+
+const reduce = (collection, reducer, initial) => {
+	let value = initial;
+
+	for (let i = 0; i < collection.length; i++) {
+		let currentValue = collection[i];
+		value = reducer(value, currentValue);
+	}
+
+	return value;
 }
+
+console.log('reduce:', reduce(collection, sumReducer, initial));
 
 module.exports = reduce;
