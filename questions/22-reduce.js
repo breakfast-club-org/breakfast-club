d30d55cf9d8ba8519a951c8056d1d9781044a187
Shawn Z <shawn.zhu@yahoo.com>
2021-02-12 10:06:38 -0800

    Shawn solutions (#46)
    * feat: 🃏
    
    * feat: 🃏
    
    * refactor: remove schedule
    
    * wo
    
    * add fisher-yates shuffle()
    
    * feat: woo methods and stuff
    
    * update shuffle to be an object instead of a class. add deck implementation
    
    * refactor: casino
    
    * Fix styles
    
    * update casino
    
    * 🎵
    
    * music
    
    * matrix transformations
    
    * maze runner not the book
    
    * cleanup
    
    * more clean up
    
    * tabs vs spaces
    
    * solution for maze runner
    
    * wow
    
    * wow
    
    * don't
    
    * linear mergeSortedArray implemenation
    
    * find dups
    
    * next next week
    
    * docs(readme): Updating with "Resources" section (#25)
    
    * typo fix
    
    * solution
    
    * solution for paren balance check
    
    * add some edge cases
    
    * averages
    
    * r
    
    * solution for 16-target-average
    
    * box
    
    * solutions for 17-storage-center
    
    * chore(deps): bump lodash from 4.17.11 to 4.17.19 (#29)
    
    Bumps [lodash](https://github.com/lodash/lodash) from 4.17.11 to 4.17.19.
    - [Release notes](https://github.com/lodash/lodash/releases)
    - [Commits](https://github.com/lodash/lodash/compare/4.17.11...4.17.19)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * tldr
    
    * tdlr not tldr
    
    * fix tests for 18-tdlr
    
    * minor fix to the tests (#31)
    
    * implement push and pop
    
    * fix tests for 18-tdlr
    
    * restore master files
    
    * restoring two files on master (#32)
    
    * implement push and pop
    
    * fix tests for 18-tdlr
    
    * restore master files
    
    * initial impl
    
    * fix some more tests
    
    * update test cases (#33)
    
    * implement push and pop
    
    * fix tests for 18-tdlr
    
    * restore master files
    
    * fix some more tests
    
    * need to use deepEqual to compare matrix arrays
    
    * fix tests (#34)
    
    * implement push and pop
    
    * fix tests for 18-tdlr
    
    * restore master files
    
    * fix some more tests
    
    * need to use deepEqual to compare matrix arrays
    
    * minor syntax change
    
    * add enhanced add function
    
    * enhanced add question (#36)
    
    * implement push and pop
    
    * fix tests for 18-tdlr
    
    * restore master files
    
    * fix some more tests
    
    * need to use deepEqual to compare matrix arrays
    
    * add enhanced add function
    
    * update test cases
    
    * fix test cases; and properly export function
    
    * add solution to #19
    
    * fix test cases throw stack trace
    
    * updated test cases (#37)
    
    * implement push and pop
    
    * fix tests for 18-tdlr
    
    * restore master files
    
    * fix some more tests
    
    * need to use deepEqual to compare matrix arrays
    
    * add enhanced add function
    
    * update test cases
    
    * fix test cases; and properly export function
    
    * fix test cases throw stack trace
    
    * add curry implementation for question 19 variant
    
    * bh
    
    * refactor: security fix
    
    * refactor: security update
    
    * tabs v space
    
    * solution for 20-browser-history
    
    * exclude
    
    * solution fro 21-exclude-things
    
    * fix test for 21-exclude-things
    
    * fix test (#40)
    
    * implement push and pop
    
    * fix tests for 18-tdlr
    
    * restore master files
    
    * fix some more tests
    
    * need to use deepEqual to compare matrix arrays
    
    * add enhanced add function
    
    * update test cases
    
    * fix test cases; and properly export function
    
    * fix test cases throw stack trace
    
    * fix test for 21-exclude-things
    
    * use match reduce
    
    * add new question
    
    * solution
    
    * update test for 22-reduce
    
    * add new question and test (#41)
    
    * implement push and pop
    
    * fix tests for 18-tdlr
    
    * restore master files
    
    * fix some more tests
    
    * need to use deepEqual to compare matrix arrays
    
    * add enhanced add function
    
    * update test cases
    
    * fix test cases; and properly export function
    
    * fix test cases throw stack trace
    
    * fix test for 21-exclude-things
    
    * add new question
    
    * update test for 22-reduce
    
    Co-authored-by: Jason Ly <jason.ly@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ questions/22-reduce.js
@@ -0,0 +1,41 @@
+/**
+ * Assume there is no Array.prototype.reduce()
+ *
+ * write a reduce(collectin, reducer, initial)
+ * where:
+ *   collection - array like data structure
+ * 	 reducer - function
+ *   initial - optional intial value
+ */
+const withValue = v => fn => arr => {
+	function recur([acc, current, ...rest]) {
+		if (!current) {
+			return acc;
+		}
+
+		let newAcc = fn(acc, current);
+
+		if (!rest.length) {
+			return newAcc;
+		}
+
+		return recur([newAcc, ...rest]);
+	}
+
+	if (v === undefined && arr.length === 0) {
+		throw new TypeError('Reduce of empty array with no initial value');
+	}
+
+	if (v !== undefined) {
+		arr = [v, ...arr];
+	}
+
+	return recur(arr);
+};
+
+function reduce(collection, reducer, intial) {
+	/* implmentation */
+	return withValue(intial)(reducer)(Array.from(collection));
+}
+
+module.exports = reduce;
