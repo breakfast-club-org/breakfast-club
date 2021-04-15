d8593fac2086e3e4a8296fd8b21c9c47e044765b
Jason Ly <jason.ly@gmail.com>
2021-04-15 11:41:01 -0700

    feat: first character not repeated
    Wooo here we go! I'm going to add some tests next.

---
+++ questions/25-first-character-not-repeated.js
@@ -0,0 +1,12 @@
+/**
+ * Write a function that accepts a string and finds the first character that isn't repeated
+ *
+ * write a firstCharacterNotRepeated(str)
+ * where:
+ *   str - string
+ */
+const firstCharacterNotRepeated = (str) => {
+	/* implmentation */
+}
+
+module.exports = firstCharacterNotRepeated;
