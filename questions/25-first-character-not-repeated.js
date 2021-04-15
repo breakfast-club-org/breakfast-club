edf3292c5ec696788bae1f5d3309ac9bbfbc4ad4
Mat Ruszczycky <mat_ruszczycky@apple.com>
2021-04-15 13:45:23 -0700

    dev: init push for q25 for mat to do stuff..and things

--- questions/25-first-character-not-repeated.js
+++ questions/25-first-character-not-repeated.js
@@ -6,7 +6,23 @@
  *   str - string
  */
 const firstCharacterNotRepeated = (str) => {
-  /* implmentation */
+  let haystack = str.replace(/\s+/g, '');
+  let haystackSorted = haystack;
+  let haystackLength = {
+    initial: haystack.length,
+    before: null,
+    after: null
+  };
+
+  for (let i = 0; i < haystackLength.initial; i++) {
+    haystackLength.before = haystackSorted.length;
+    haystackSorted = haystackSorted.replace(new RegExp(haystack[i], 'g'), '');
+    haystackLength.after = haystackSorted.length;
+
+    if (haystackLength.before - haystackLength.after === 1) {
+      return haystack[i];
+    }
+  }
 }
 
 module.exports = firstCharacterNotRepeated;
