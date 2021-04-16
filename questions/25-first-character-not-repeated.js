4c816ba63a559f56b4a10c3ea426b6951889e596
Mat Ruszczycky <mat_ruszczycky@apple.com>
2021-04-15 20:22:18 -0700

    dev: refactor

--- questions/25-first-character-not-repeated.js
+++ questions/25-first-character-not-repeated.js
@@ -9,11 +9,11 @@
   // Gut the string of all white space
   let haystack = str.replace(/\s+/g, '');
 
-  // Check for a second index instance of each char in string starting from the beginning - return once a second index is not found -1
-  for (let i = 0, len = haystack.length; i < len; i++) {
-    if(haystack.indexOf(haystack[i], haystack.indexOf(haystack[i]) + 1) === -1) {
-      return haystack[i];
-    }
+  // Check for a second index instance of each char in string starting from the beginning, return once a second index is not found -1
+  for (needle of haystack) {
+      if(haystack.indexOf(needle, haystack.indexOf(needle) + 1) === -1) {
+        return needle;
+      }
   }
 }
 
