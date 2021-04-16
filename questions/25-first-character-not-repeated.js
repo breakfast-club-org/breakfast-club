fc0ef79f9896516d1de2162097c7c4a0a384cd72
Mat Ruszczycky <mat_ruszczycky@apple.com>
2021-04-15 20:47:27 -0700

    dev: refactor

--- questions/25-first-character-not-repeated.js
+++ questions/25-first-character-not-repeated.js
@@ -6,8 +6,8 @@
  *   str - string
  */
  const firstCharacterNotRepeated = (str) => {
-  // Gut the string of all white space
-  let haystack = str.replace(/\s+/g, '');
+  // Gut the string of all white space & special chars
+  let haystack = str.replace(/[^a-zA-Z0-9]/g, '');
 
   // Check for a second index instance of each char in string starting from the beginning, return once a second index is not found -1
   for (needle of haystack) {
