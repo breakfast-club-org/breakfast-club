bb7048d65966aa2feb3deffd322fd092d17b7e69
JasonLy <jason.ly@gmail.com>
2021-04-30 06:16:18 -0700

    feat: oh no

--- questions/25-first-character-not-repeated.js
+++ questions/25-first-character-not-repeated.js
@@ -7,6 +7,23 @@
  */
 const firstCharacterNotRepeated = (str) => {
   /* implmentation */
+  const storage = {};
+
+  for (let i = 0; i < str.length; i++) {
+    let character = str[i];
+
+    if (storage[character]) {
+      storage[character] = storage[character] + 1;
+    } else {
+      storage[character] = 1;
+    }
+
+    for (character in storage) {
+      if (storage[character] === 1) {
+        return character;
+      }
+    }
+  }
 }
 
 module.exports = firstCharacterNotRepeated;
