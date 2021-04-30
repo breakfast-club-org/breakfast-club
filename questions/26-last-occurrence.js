20f273303be6176f843abc2bf20094fa93e6d562
Jason Ly <jason.ly@gmail.com>
2021-04-30 15:41:46 -0700

    Update 26-last-occurrence.js

--- questions/26-last-occurrence.js
+++ questions/26-last-occurrence.js
@@ -1,5 +1,6 @@
 /**
  * Write a function that accepts a string and a target, and returns the last occurrence of an element in a string
+ * btw: you're not allowed to use `lastIndexOf`
  *
  * write a lastOccurrence(str)
  * where:
