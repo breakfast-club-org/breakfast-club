f08a9a7365860ca441fda9757fe4c25eaa920e15
Mat Ruszczycky <mat_ruszczycky@apple.com>
2021-05-07 11:39:40 -0700

    dev: alt solution for 26

--- questions/26-last-occurrence.js
+++ questions/26-last-occurrence.js
@@ -7,13 +7,28 @@
  *   str - string
  *   target - string
  */
- const lastOccurrence = (str, target) => {   
+//  const lastOccurrence = (str, target) => {   
+//   if (str.includes(target)) {
+//     const len = str.match(new RegExp(target, 'g')).length;
+//     return len === 1 ? str.indexOf(target) : str.indexOf(target, (str.indexOf(target) + len - 1));
+//   }
+
+//   return -1;
+// };
+
+const lastOccurrence = (str, target) => {   
   if (str.includes(target)) {
-    const len = str.match(new RegExp(target, 'g')).length;
-    return len === 1 ? str.indexOf(target) : str.indexOf(target, (str.indexOf(target) + len - 1));
+  	let indexOf = 0;
+    
+    for (let i = 0, len = str.length; i < len; i++) {
+    	indexOf = (str[i] === target && i > indexOf) ? i : indexOf;
+    }
+
+    return indexOf;
   }
 
   return -1;
 };
+
 
 module.exports = lastOccurrence;
