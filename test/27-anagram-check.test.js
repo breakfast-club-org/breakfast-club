8f87a5f1c5be4fa3d4998a95280ed2a5d640d3da
Jason Ly <jason.ly@gmail.com>
2021-05-21 13:37:49 -0700

    fix bugs

--- test/27-anagram-check.test.js
+++ test/27-anagram-check.test.js
@@ -1,26 +1,35 @@
 const chai = require('chai');
 const assert = chai.assert;
-const anagramCheck = require('../questions/26-last-occurrence');
+const anagramCheck = require('../questions/27-anagram-check');
 
 describe('Method: anagramCheck tests', function() {
   it('string "knee" vs "keen" should return true', function() {
     const result = anagramCheck('knee', 'keen');
-    const expected = true;
 
-    assert.equal(result, expected);
+    assert.isTrue(result);
+  });
+  
+  it('string "knee" vs "knee" should return false', function() {
+    const result = anagramCheck('knee', 'knee');
+
+    assert.isFalse(result);
+  });
+  
+  it('number 1 vs 1 should return false', function() {
+    const result = anagramCheck(1, 1);
+
+    assert.isFalse(result);
   });
 
   it('strings of different lengths should return false', function() {
     const result = anagramCheck('longer', 'shorter');
-    const expected = false;
 
-    assert.equal(result, expected);
+    assert.isFalse(result);
   });
 
   it('string "wow" vs "woo" should return false', function() {
     const result = anagramCheck('wow', 'woo');
-    const expected = -1;
 
-    assert.equal(result, expected);
+    assert.isFalse(result);
   });
 });
