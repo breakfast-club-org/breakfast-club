a69816d3d149818bbd1a8359743067aeb1a3b613
Brian Thiel <bthiel@apple.com>
2021-05-24 10:35:34 -0500

    removed log

--- questions/25-first-character-not-repeated.js
+++ questions/25-first-character-not-repeated.js
@@ -11,7 +11,6 @@ const firstCharacterNotRepeated = (str) => {
   for (let i = 0; i < str.length; i++) {
     let char = str.charAt(i);
     if (str.indexOf(char) === i && str.indexOf(char, i + 1) === -1) {
-      console.log('first character not repeated is: ', char);
       return char;
     }
   }
