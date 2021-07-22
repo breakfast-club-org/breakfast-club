c076c21e4c7547236e63fed571e14a8fa2759052
JasonLy <jason.ly@gmail.com>
2021-07-22 00:01:00 -0700

    feat: add ts solution

---
+++ questions/27-anagram-check.ts
@@ -0,0 +1,39 @@
+/**
+ * Write a function that accepts two strings, and checks if they're anagrams of each other.
+ * A word is an anagram of another word if both use the same letters in the same quantity, but are arranged differently.
+ */
+
+interface AnagramProps {
+  strOne: String,
+  strTwo: String,
+}
+
+// const anagramCheck = ({strOne, strTwo}: AnagramProps): Boolean => {
+//   /* implmentation */
+// }
+
+const mapCharacters = (str: String) => {
+  let storage = {};
+
+  for (let character of str) {
+    storage[character] = storage[character] + 1 || 1;
+  }
+
+  return storage;
+}
+  
+const anagramCheck = ({strOne, strTwo}: AnagramProps) => {
+  const strOneMap = mapCharacters(strOne);
+  const strTwoMap = mapCharacters(strTwo);
+
+  for (let char in strOneMap) {
+    if (strOneMap[char] !== strTwoMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+module.exports = anagramCheck;
+  
