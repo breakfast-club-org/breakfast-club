cbe91745235a45ca04239a6d5ab1ea7c766fc88a
Jason Ly <jason.ly@gmail.com>
2021-07-29 13:48:16 -0700

    whoops fixed implementation

--- questions/28-curry-add.ts
+++ questions/28-curry-add.ts
@@ -1,13 +1,19 @@
 /**
- * Write a function that accepts a function and transforms it's usage from add(a, b, c) to addChain(a)(b)(c) or addChain(a, b, c)
+ * Write a function, curryChain, that accepts a function and transforms it's usage from add(a, b, c) to addChain(a)(b)(c) or addChain(a, b, c)
+ * example:
+ * const addChain = curryChain(add);
+ * addChain(1, 2, 3)
+ * addChain(1)(2)(3)
  */
 
 const add = (a, b, c) => {
   return a + b + c;
 }
 
-const addChain = (f) => {
+const curryChain = (f) => {
   /* implmentation */
 }
+
+const addChain = curryChain(add);
 
 module.exports = addChain;
