6d769b5c8973d1ac49147c1240b26246afd6a531
Brian Thiel <bthiel@apple.com>
2021-08-05 11:08:32 -0500

    solved curry

--- questions/28-curry-add.ts
+++ questions/28-curry-add.ts
@@ -4,16 +4,17 @@
  * const addChain = curryChain(add);
  * addChain(1, 2, 3)
  * addChain(1)(2)(3)
+ *
+ * https://github.com/learn-javascript-courses/es6-curry
  */
 
-const add = (a, b, c) => {
-  return a + b + c;
-}
-
-const curryChain = (f) => {
-  /* implmentation */
-}
-
+const add = (a, b, c) => a + b + c;
+const curryChain = (f, arr = []) => (...args) => (a => a.length === f.length ? f(...a) : curryChain(f, a))([...arr, ...args]);
 const addChain = curryChain(add);
+
+// console.log('addChain', addChain(1)(2)(3));
+// console.log('addChain', addChain(1, 2, 3));
+// console.log('addChain', addChain(1, 2)(3));
+// console.log('addChain', addChain(1)(2, 3));
 
 module.exports = addChain;
