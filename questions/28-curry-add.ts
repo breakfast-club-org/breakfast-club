b27b8962ad814e5b31c6798ad58c1e5028ea808c
Jason Ly <jason.ly@gmail.com>
2021-08-05 10:59:08 -0700

    yay

--- questions/28-curry-add.ts
+++ questions/28-curry-add.ts
@@ -6,13 +6,49 @@
  * addChain(1)(2)(3)
  */
 
-const add = (a, b, c) => {
-  return a + b + c;
+
+// const add = (a, b, c) => {
+//     return a + b + c;
+// }
+
+// const curryChain = (f) => {
+//     const curryFunc = (...args) => {
+//         console.log(...args)
+//         if (args.length >= f.length) {
+//             return f.apply(this, args);
+//         } else {
+//             return function(...args2) {
+//                 return curryFunc.apply(this, args.concat(args2));
+//             }
+//         }
+//     }
+//     return curryFunc;
+// };
+
+interface AddProps {
+    a: number,
+    b: number,
+    c: number
 }
 
-const curryChain = (f) => {
-  /* implmentation */
+const add = ({a, b, c}: AddProps): number => {
+    return a + b + c;
 }
+
+const curryChain = (f: Function): Function => {
+
+    const curryFunc = (...args: [number]): Function | number => {
+        if (args.length >= f.length) {
+            return f.apply(this, args);
+        } else {
+            return function(...args2) {
+                return curryFunc.apply(this, args.concat(args2));
+            }
+        }
+    }
+
+    return curryFunc;
+};
 
 const addChain = curryChain(add);
 
