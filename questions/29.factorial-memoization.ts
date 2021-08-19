052c1b3fddd30154c68be86b869fb1ccfb747bb2
Jason Ly <jason.ly@gmail.com>
2021-08-19 13:35:50 -0700

    Update 29.factorial-memoization.ts

--- questions/29.factorial-memoization.ts
+++ questions/29.factorial-memoization.ts
@@ -9,6 +9,7 @@ export function factorial(n) {
 /**
  * Implement a function called memoize that can cache the solutions of factorial to improve the
  * efficency of the function
+ * const memoizeFactorial = memoize(factorial);
  * ex: memoizedFactorial(5), 5 * 4 * 3 * 2 * 1  = 120 // should be faster the second time it's called in comparison
  */
 export function memoize(fn) {
