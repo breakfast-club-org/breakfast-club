cc08d50dd23fcea51da3b21dfad6faada91ca9f7
Jason Ly <jason.ly@gmail.com>
2021-08-19 12:32:43 -0700

    Create 29.factorial-memoization.ts

---
+++ questions/29.factorial-memoization.ts
@@ -0,0 +1,16 @@
+/**
+ * Implement a function called fibonacci that accepts a number and returns the fibonacci number
+ * ex: factorial(5), 5 * 4 * 3 * 2 * 1  = 120
+ */
+export function factorial(n) {
+
+}
+
+/**
+ * Implement a function called memoize that can cache the solutions of factorial to improve the
+ * efficency of the function
+ * ex: memoizedFactorial(5), 5 * 4 * 3 * 2 * 1  = 120 // should be faster the second time it's called in comparison
+ */
+export function memoize(fn) {
+
+}
