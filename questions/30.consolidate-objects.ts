ee9ed6ad94203b98424879e0fd7218cc2d127f0f
Shawn Z <shawn.zhu@yahoo.com>
2021-09-13 09:48:15 -0700

    solve for 30.sonsolidate-objects

--- questions/30.consolidate-objects.ts
+++ questions/30.consolidate-objects.ts
@@ -6,22 +6,37 @@
  *   something: 'ok',
  *   other: 1
  * }
- * 
+ *
  * const obj2 = {
  *   thing: 'huh ok',
  *   other: 2
  * }
- * 
+ *
  * consolidateObjects(obj1, obj2)
- * 
- * // should return 
+ *
+ * // should return
  * {
  *   thing: ['wow', 'huh ok'],
  *   something: 'ok',
  *   other: [1, 2]
  * }
  */
+type Primitives = string | number | boolean;
+type PrimObj = Record<string, Primitives>;
+export default function consolidateObjects(objOne: PrimObj, objTwo: PrimObj): Record<string, unknown> {
+    const ro: Record<string, unknown> = {...objOne};
 
-export function consolidateObjects(objOne, objTwo) {
+    Object
+        .entries(objTwo)
+        .forEach(([k, v]) => {
+            const valueInRO = ro[k]; // O(1)
+            if (valueInRO) {
+                // O(1) because only two objects and each key has a max of two values
+                ro[k] = Array.isArray(valueInRO) ? [...valueInRO, v] : [valueInRO, v];
+            } else {
+                ro[k] = v;
+            }
+        });
 
+    return ro;
 }
