71294f8eddd592e0578b9d2eb90c7a87269a7011
Jason Ly <jason.ly@gmail.com>
2021-09-23 10:05:37 -0700

    merging main to solutions (#70)
    * 👌
    
    * woo

---
+++ questions/30.consolidate-objects.ts
@@ -0,0 +1,27 @@
+/**
+ * Write a function to consolidate values in two objects
+ * ex:
+ * const obj1 = {
+ *   thing: 'wow',
+ *   something: 'ok',
+ *   other: 1
+ * }
+ * 
+ * const obj2 = {
+ *   thing: 'huh ok',
+ *   other: 2
+ * }
+ * 
+ * consolidateObjects(obj1, obj2)
+ * 
+ * // should return 
+ * {
+ *   thing: ['wow', 'huh ok'],
+ *   something: 'ok',
+ *   other: [1, 2]
+ * }
+ */
+
+export function consolidateObjects(objOne, objTwo) {
+
+}
