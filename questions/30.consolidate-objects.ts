0ae7caf7dbf0b0e81ab05a33fea279f8e31ad902
Brian Thiel <bthiel@apple.com>
2021-09-23 10:10:48 -0500

    solved ?? maybe haha

--- questions/30.consolidate-objects.ts
+++ questions/30.consolidate-objects.ts
@@ -6,15 +6,15 @@
  *   something: 'ok',
  *   other: 1
  * }
- * 
+ *
  * const obj2 = {
  *   thing: 'huh ok',
  *   other: 2
  * }
- * 
+ *
  * consolidateObjects(obj1, obj2)
- * 
- * // should return 
+
+ * // should return
  * {
  *   thing: ['wow', 'huh ok'],
  *   something: 'ok',
@@ -22,6 +22,27 @@
  * }
  */
 
-export function consolidateObjects(objOne, objTwo) {
-
+const obj1 = {
+	thing: 'wow',
+	something: 'ok',
+	other: 1
 }
+
+const obj2 = {
+	thing: 'huh ok',
+	other: 2
+}
+
+export function consolidateObjects(...arr) {
+	const result = {};
+	arr.forEach(item => {
+		Object.keys(item).forEach(key => {
+			if (!result[key]) result[key] = [];
+			result[key].push(item[key]);
+		});
+	});
+	return result;
+}
+
+console.log('consolidateObjects(obj1, obj2)', consolidateObjects(obj1, obj2));
+
