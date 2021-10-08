3e4448e7b95848dde66c2812491a9e475bd4fa91
Shawn Z <shawn.zhu@yahoo.com>
2021-10-08 11:29:57 -0700

    solution for 31

--- questions/31-to-the-front.ts
+++ questions/31-to-the-front.ts
@@ -2,9 +2,9 @@
  * Write a function to move elements of an array a number of times
  * ex:
  * const input = [1,2,3,4,5,6,7]
- * 
+ *
  * toThefront(input, 4)
- * 
+ *
  * // should return
  * 1st: [7,1,2,3,4,5,6]
  * 2nd: [6,7,1,2,3,4,5]
@@ -12,6 +12,27 @@
  * 4th: [4,5,6,7,1,2,3] <- result
  */
 
-export function toThefront(arr, rotations) {
+// fastest when < 10 rotations with input of 1000 - 7% faster
+export default function toThefront(arr: number[], rotations: number): number[] {
+    if (!arr.length) {
+        return [];
+    }
 
+    for (let i=rotations; i > 0; i--) {
+        arr.splice(0, 0, arr.pop());
+    }
+
+    return arr;
+}
+
+// fastest when > 100 rotations with input of 1000 - 7% faster
+export function toTheFront2(arr: number[], rotations: number): number[] {
+	const out = Array(rotations);
+	for (let i=arr.length - 1, o = out.length - 1, x = rotations; x > 0; x--, i--, o-- ) {
+        out[o] = arr[i];
+    }
+    for (let i=0, o=out.length; i < arr.length - rotations; i++, o++) {
+        out[o] = (arr[i]);
+    }
+    return out;
 }
