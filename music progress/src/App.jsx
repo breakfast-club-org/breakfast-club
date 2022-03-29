f20789f863339bf8775743c19f85e09ec4f4271d
Sharon Paz <oxleberry@gmail.com>
2022-03-29 10:15:44 -0700

    add play and pause functionality

--- music progress/src/App.jsx
+++ music progress/src/App.jsx
@@ -1,36 +1,35 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [buttonIcon, setButtonIcon] = useState('play');
+
+	const clickHandler = () => {
+		const audio = document.getElementById('audio');
+		if (isPlaying) {
+			audio.pause();
+			setIsPlaying(false);
+			setButtonIcon('play');
+		} else {
+			audio.play();
+			setIsPlaying(true);
+			setButtonIcon('pause');
+		}
+	}
 
   return (
     <div className="App">
 			<div className="audio-player">
 				<div className="title-controls">
-					<h2 className="title">Play</h2>
+					<h2 className="title">Mah N&agrave; Mah N&agrave;</h2>
 					<div className="controls">
-						<button className="play"></button>
+						<button onClick={clickHandler} className={buttonIcon}></button>
 					</div>
-				</div>
-				<div className="progress-bar"></div>
-			</div>
-
-			<div className="audio-player">
-				<div className="title-controls">
-					<h2 className="title">Pause</h2>
-					<div className="controls">
-						<button className="pause"></button>
-					</div>
-				</div>
-				<div className="progress-bar"></div>
-			</div>
-
-			<div className="audio-player">
-				<div className="title-controls">
-					<h2 className="title">Replay</h2>
-					<div className="controls">
-						<button className="replay"></button>
-					</div>
+					<audio id="audio"
+								src="./src/audio/mah-na-mah-na.m4r">
+									Your browser does not support the <code>audio</code> element.
+					</audio>
 				</div>
 				<div className="progress-bar"></div>
 			</div>
