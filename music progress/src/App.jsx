0dd0c43ae7ed3c2ada37808dc6472dbb4684244a
Sharon Paz <oxleberry@gmail.com>
2022-03-28 17:50:32 -0700

    basic layout and styles for audio player

--- music progress/src/App.jsx
+++ music progress/src/App.jsx
@@ -5,7 +5,35 @@ function App() {
 
   return (
     <div className="App">
+			<div className="audio-player">
+				<div className="title-controls">
+					<h2 className="title">Play</h2>
+					<div className="controls">
+						<button className="play"></button>
+					</div>
+				</div>
+				<div className="progress-bar"></div>
+			</div>
 
+			<div className="audio-player">
+				<div className="title-controls">
+					<h2 className="title">Pause</h2>
+					<div className="controls">
+						<button className="pause"></button>
+					</div>
+				</div>
+				<div className="progress-bar"></div>
+			</div>
+
+			<div className="audio-player">
+				<div className="title-controls">
+					<h2 className="title">Replay</h2>
+					<div className="controls">
+						<button className="replay"></button>
+					</div>
+				</div>
+				<div className="progress-bar"></div>
+			</div>
     </div>
   )
 }
