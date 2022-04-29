938cf197e7d83cda3d4cac330752f63c39430466
Brian Thiel <bthiel@apple.com>
2022-04-29 16:29:56 -0500

    autoplay if already playing

--- music progress/src/components/MusicPlayer.jsx
+++ music progress/src/components/MusicPlayer.jsx
@@ -31,6 +31,15 @@ export default function MusicPlayer({ playlist }) {
 		} else {
 			pauseAudio();
 		}
+	}, [audioSrc]);
+
+	useEffect(() => {
+		// play or pause audio
+		if (isPlaying) {
+			playAudio();
+		} else {
+			pauseAudio();
+		}
 		// listen for events when isPlaying state changes
 		audioEvents();
 
@@ -83,7 +92,9 @@ export default function MusicPlayer({ playlist }) {
 		// subtract 1 from length because we start at 0
 		if (playlistCounter < playlistTotal - 1) {
 			setPlaylistCounter(prevState => prevState + 1);
-			setIsPlaying(false);
+			if (!isPlaying) {
+				setIsPlaying(false);
+			}
 		}
 	}
 
